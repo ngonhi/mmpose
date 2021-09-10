284fead49040c91db5f84dfd35c2340ff759f87e
ngonhi <ngonhi@grinnell.edu>
2021-09-10 22:50:05 +0700

    env

---
+++ tools/ID_card_train.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright (c) OpenMMLab. All rights reserved.
+
+CONFIG=~/ID_Card/mmpose/configs/ID_card/overfit_higherhrnet_w32_IDCard_512x512.py
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python $(dirname "$0")/train.py $CONFIG
