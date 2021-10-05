97e52534d02567e713d2184bed8d42e03238ceb9
ngonhi <ngonhi@grinnell.edu>
2021-10-06 00:44:28 +0700

    fix train metricss

--- mmpose/core/utils/compute_train_metrics.py
+++ mmpose/core/utils/compute_train_metrics.py
@@ -1,48 +1,51 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 import tempfile
-import sys
+import warnings
 
-from mmcv.runner import HOOKS, Hook
+from mmcv.runner import EvalHook as _EvalHook
 
-@HOOKS.register_module()
-class ComputeTrainMetricsHook(Hook):
-    def __init__(self, dataloader):
-        self.results = []
-        self.dataloader = dataloader
+MMPOSE_GREATER_KEYS = [
+    'acc', 'ap', 'ar', 'pck', 'auc', '3dpck', 'p-3dpck', '3dauc', 'p-3dauc'
+]
+MMPOSE_LESS_KEYS = ['loss', 'epe', 'nme', 'mpjpe', 'p-mpjpe', 'n-mpjpe']
 
-    def clear(self):
-        self.results = []
 
-    def before_train_epoch(self, runner):
-        self.clear()
+class ComputeTrainMetricsHook(_EvalHook):
+    def __init__(self,
+                 dataloader,
+                 start=None,
+                 interval=1,
+                 by_epoch=True,
+                 save_best=None,
+                 rule=None,
+                 test_fn=None,
+                 greater_keys=MMPOSE_GREATER_KEYS,
+                 less_keys=MMPOSE_LESS_KEYS,
+                 **eval_kwargs):
 
-    # Compute metrics after epoch
-    def after_train_iter(self, runner):
-        result = runner.outputs['results']
-        saved_result = []
-        # Rescale to original dataset scale
-        for idx, sample in enumerate(result):
-            saved_sample = {}
-            saved_sample['scores'] = sample['scores']
-            saved_sample['image_paths'] = sample['image_paths']
-            saved_sample['preds'] = sample['preds']
-            rescale = sample['rescale']
-            if len(sample['preds']) > 0 and rescale:
-                for i, item in enumerate(saved_sample['preds']):
-                    saved_sample['preds'][i][:, 0] = saved_sample['preds'][i][:, 0] * rescale[0]
-                    saved_sample['preds'][i][:, 1] = saved_sample['preds'][i][:, 1] * rescale[1]
-            saved_result.append(saved_sample)
-        self.results += saved_result
-        print('size of output results after iter', sys.getsizeof(self.results))
+        if test_fn is None:
+            from mmpose.apis import single_gpu_test
+            test_fn = single_gpu_test
 
-    def after_train_epoch(self, runner):
-        # Compute metrics
-        print('size of output results after epoch', sys.getsizeof(self.results))
+        super().__init__(dataloader, start, interval, by_epoch, save_best,
+                         rule, test_fn, greater_keys, less_keys, **eval_kwargs)
+
+    def evaluate(self, runner, results):
+        """Evaluate the results.
+
+        Args:
+            runner (:obj:`mmcv.Runner`): The underlined training runner.
+            results (list): Output results.
+        """
         with tempfile.TemporaryDirectory() as tmp_dir:
             eval_res = self.dataloader.dataset.evaluate(
-                self.results,
+                results,
                 res_folder=tmp_dir,
-                logger=runner.logger)
-        
+                logger=runner.logger,
+                **self.eval_kwargs)
+
         for name, val in eval_res.items():
             runner.log_buffer.output[name+'_train'] = val
-        self.clear()
+        runner.log_buffer.ready = True
+
+        return None
